2772ce850fe5d491e46b62fc34e4bbcdd0cfe96f
skwidgets <sbrown@skwidgets.com>
2022-08-18 22:58:10 -0600

    added cart and cart items

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -8,7 +8,7 @@ import {
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
-import { UserContext } from "../contexts/user.context";
+import { UserContext } from "../../contexts/user.context";
 
 import "./sign-up-form.styles.scss";
 
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,15 +1,20 @@
 import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 
+import CartIocn from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+
 import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
-import { UserContext } from "../../components/contexts/user.context";
+import { UserContext } from "../../contexts/user.context";
 
 import { auth, signOutUser } from "../../utils/firebase/firebase.utils";
 
 import "./navigation.styles.scss";
+import { CartContext } from "../../contexts/cart.context";
 
 const Navigation = () => {
-  const { currentUser, setCurrentUser } = useContext(UserContext);
+  const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
   return (
     <Fragment>
@@ -30,8 +35,10 @@ const Navigation = () => {
               Sign In
             </Link>
           )}
+          <CartIocn />
         </div>
       </div>
+      {isCartOpen && <CartDropdown />}
       <Outlet />
     </Fragment>
   );
